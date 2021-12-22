44f332fb4c8c491fcd82ea1215f99cbe4a932c8d
zhangyc <zhangyc@yuanfudao.com>
2021-12-22 20:15:16 +0800

    feat: roman to digital

--- roman.ts
+++ roman.ts
@@ -14,6 +14,11 @@ const ROMAN_LETTERS = [
     [1, 'I'],
 ];
 
+const LETTER_INDEXES = ROMAN_LETTERS.map(x => x[1]).reduce((memo, curr, index) => {
+    memo[curr] = index;
+    return memo;
+}, {});
+
 export function digitalToRoman(digital: number) {
     let result = '';
     ROMAN_LETTERS.forEach(([base, letter]) => {
@@ -22,6 +27,22 @@ export function digitalToRoman(digital: number) {
             digital -= base as number;
         }
     });
+
+    return result;
+}
+
+export function romanToDigital(roman: string) {
+    let result = 0;
+    for (let i = 0; i < roman.length; i++) {
+        const currentIndex = LETTER_INDEXES[roman[i]];
+        const nextIndex = i === roman.length - 1 ? ROMAN_LETTERS.length : LETTER_INDEXES[roman[i+1]];
+        let num = ROMAN_LETTERS[currentIndex][0] as number;
+        if (currentIndex > nextIndex) {
+            num = 0 - num;
+        }
+
+        result += num;
+    }
 
     return result;
 }
